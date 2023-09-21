b5d2afcbe56c3f0b25342445c4ba8391de669dd6
Priyanshu vijay Pawar <priyanshupawar2021@gmail.com>
2023-09-21 20:52:15 +0530

    updates

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//b0429937d3b8cd1f196f94f26dbc62d1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
