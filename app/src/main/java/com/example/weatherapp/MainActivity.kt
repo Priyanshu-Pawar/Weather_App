0c5bdde2edd18d2a5f6b7dae5577b30091b678c9
Priyanshu vijay Pawar <priyanshupawar2021@gmail.com>
2023-10-18 20:38:30 +0530

    add current location feature.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.weatherapp
 
+import android.Manifest
 import android.app.ProgressDialog
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
     private lateinit var progressDialog: ProgressDialog
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,17 +41,52 @@ class MainActivity : AppCompatActivity() {
         progressDialog.setMessage("Loading...")
         progressDialog.setCancelable(false)
 
-        fetchWeatherData("India")
-        searchCity()
+//        fetchWeatherData("India")
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this) // Initialize fusedLocationClient
 
+        searchCity()
+        fetchCurrentLocationWeather()
+
+    }
+    private fun fetchCurrentLocationWeather() {
+        if (checkLocationPermission()) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val geocoder = Geocoder(this@MainActivity, Locale.getDefault())
+                    try {
+                        val addresses: MutableList<Address>? = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                        if (addresses != null) {
+                            if (addresses.isNotEmpty()) {
+                                val city = addresses[0].locality
+                                fetchWeatherData(city)
+                            }
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        val fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+        return if (ContextCompat.checkSelfPermission(this, fineLocationPermission) == PackageManager.PERMISSION_GRANTED) {
+            true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(fineLocationPermission), 1)
+            false
+        }
     }
 
     private fun searchCity(){
         val searchView = binding.searchView
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if(query != null){
-                 fetchWeatherData(query)
+                if (!query.isNullOrBlank()) {
+                    // When the user submits the search query, fetch weather data for the location
+                    fetchWeatherData(query)
+                    searchView.clearFocus() // Hide the keyboard
                 }
                 return true
             }
@@ -57,6 +104,7 @@ class MainActivity : AppCompatActivity() {
             .build().create(ApiInterface::class.java)
 
           val response = retrofit.getWeatherData(CityName, "b0429937d3b8cd1f196f94f26dbc62d1" , "metric")
+
 
          response.enqueue(object: Callback<WeatherAppData>{
              override fun onResponse(
